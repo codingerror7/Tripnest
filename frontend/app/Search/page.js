c65e8cbd29d2a13ce05b7f563e4940bea211ba89
sujal <saraswatsujal@gmail.com>
2025-12-30 16:08:51 +0530

    minor commit

--- frontend/app/Search/page.js
+++ frontend/app/Search/page.js
@@ -1,5 +1,5 @@
-export const dynamic = "force-dynamic";
 "use client"
+export const dynamic = "force-dynamic";
 import React from 'react'
 import { useState } from 'react'
 import { useSearchParams } from 'next/navigation'
